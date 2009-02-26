70780775437799c8988c7029db238799c153615d
barla@PC-NB-Barla <barla@PC-NB-Barla>
2009-02-27 00:08:54 +0100

    now it also works for the root page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
   acts_as_nested_set
+  validates_uniqueness_of :sid, :scope => :parent_id
 
   has_many :page_parts, :dependent => :destroy
 
@@ -7,27 +8,27 @@ class Page < ActiveRecord::Base
     path = [nil] if path.empty?
     current = traverse_path path
     if current.nil?
-      parent = traverse_path path[0..path.size - 2]
+      parent = nil
+      parent = traverse_path path[0..path.size - 2] if path.size > 1
       if parent.nil?
-        return nil  #we do not want to create new page in the middle of the tree
-      else
-        #TODO: check for permission
-        current = Page.new(:title => path.last, :sid => path.last)
-        current.save!
-        current.move_to_child_of parent #we need to save the page before in order to call this
-        #create parts
-        #TODO: co bolo skorej, vajce alebo sliepka? PagePart alebo jej revizia?
-        first_revision = PagePartRevision.create(:user => current_user, :body => "This page does not hold any content yet", :page_part_id => 0)
-        page_part = PagePart.create(:name => "body", :page => current, :current_page_part_revision => first_revision)
-        first_revision.page_part = page_part
+        return nil unless path.size == 1 && Page.root.nil?  #we do not want to create new page in the middle of the tree, but we proceed if this is going to be the root page
       end
+        #TODO: check for permission
+        ActiveRecord::Base.transaction do
+          current = Page.create(:title => path.last, :sid => path.last)
+          current.move_to_child_of parent unless parent.nil?
+          #TODO: co bolo skorej, vajce alebo sliepka? PagePart or its revision?
+          first_revision = PagePartRevision.create(:user => current_user, :body => "This page does not hold any content yet", :page_part_id => 0)
+          page_part = PagePart.create(:name => "body", :page => current, :current_page_part_revision => first_revision)
+          first_revision.page_part = page_part
+        end
     end
     return current
-    end
+  end
 
   def self.traverse_path path
     path = [nil] if path.empty?
-    parent_id = 0
+    parent_id = nil
     for chunk in path
       current = Page.find_by_parent_id_and_sid(parent_id, chunk)
       return nil if current.nil?
