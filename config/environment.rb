503eb7acc58c06173a37cfecffd6fc14044b7f22
johno@johno-laptop-vbox-ubuntu <johno@johno-laptop-vbox-ubuntu>
2009-03-01 15:52:47 +0100

    Added dependencies.

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "cucumber"
+  config.gem "webrat"
   config.gem 'maruku'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
