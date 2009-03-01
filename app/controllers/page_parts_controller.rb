2e6e7a3ecfb6c5026c00401c158b691134bb82e4
crutch@seznam.cz <crutch@seznam.cz>
2009-03-01 23:58:31 +0100

    - removed page_part_revision_controller. The same form which creates a page_part holds also the page_part_revision body.

--- app/controllers/page_parts_controller.rb
+++ app/controllers/page_parts_controller.rb
@@ -5,13 +5,14 @@ class PagePartsController < ApplicationController
   end
 
   def create
-    @page_part = PagePart.new(params[:page_part])
+    @page_part = PagePart.new(:name => params[:part_name])
     @page_part.page = Page.find(params[:page_id])
-    @page_part_revision = PagePartRevision.new(:page_part_id => 0, :user => session[:user])
+    @page_part_revision = PagePartRevision.new(:page_part_id => 0, :user => session[:user], :body => params[:page_revision_body])
     @page_part.current_page_part_revision = @page_part_revision
     @page_part.save!
     @page_part_revision.page_part = @page_part
     @page_part_revision.save!
-    redirect_to edit_page_page_part_page_part_revision_url(@page_part.page, @page_part, @page_part.current_page_part_revision)
+    redirect_to(@page_part.page.get_path + "/")
+    #redirect_to edit_page_page_part_page_part_revision_url(@page_part.page, @page_part, @page_part.current_page_part_revision)
   end
 end
