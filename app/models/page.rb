efcf729e370695b76ee1a01884bb93d655263e68
barla@PC-NB-Barla <barla@PC-NB-Barla>
2009-03-01 02:06:40 +0100

    - creation of new pages is not done automatically when user types-in a non-existing URL. Instead, a new page form is displayed. - very basic check, whether the user is authenticated, just to pass the feature test (which I can not execute because of some strange gem hell)

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,30 +4,8 @@ class Page < ActiveRecord::Base
 
   has_many :page_parts, :dependent => :destroy
 
-  def self.find_by_path path, current_user
-    path = [nil] if path.empty?
-    current = traverse_path path
-    if current.nil?
-      parent = nil
-      parent = traverse_path path[0..path.size - 2] if path.size > 1
-      if parent.nil?
-        return nil unless path.size == 1 && Page.root.nil?  #we do not want to create new page in the middle of the tree, but we proceed if this is going to be the root page
-      end
-      #TODO: check for permission
-      transaction do
-        current = Page.create(:title => path.last, :sid => path.last)
-        current.move_to_child_of parent unless parent.nil?
-        #TODO: co bolo skorej, vajce alebo sliepka? PagePart or its revision?
-        first_revision = PagePartRevision.create(:user => current_user, :body => "This page does not hold any content yet", :page_part_id => 0)
-        page_part = PagePart.create(:name => "body", :page => current, :current_page_part_revision => first_revision)
-        first_revision.page_part = page_part
-      end
-    end
-    return current
-  end
-
-  def self.traverse_path path
-    path = [nil] if path.empty?
+  def self.find_by_path path
+    path = [""] + path
     parent_id = nil
     for chunk in path
       current = Page.find_by_parent_id_and_sid(parent_id, chunk)
@@ -38,6 +16,6 @@ class Page < ActiveRecord::Base
   end
 
   def get_path
-    self.ancestors.collect {|node| node.sid}.join('/') + sid
+    self.ancestors.collect {|node| node.sid}.join('/') + "/" + self[:sid]
   end
 end
