9f2450daf7ffe7ef95391c48717a9106f503261a
Michal Barla <crutch@seznam.cz>
2009-03-12 19:01:48 +0100

    - groups can be public, so we needed to distinguish view and edit permission in its administration

--- app/models/group_permission.rb
+++ app/models/group_permission.rb
@@ -2,11 +2,25 @@ class GroupPermission < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
-  def switch
+  def switch_view
+    if can_view?
+      self.can_view = false
+      unless self.can_edit?
+        self.destroy
+      end
+    else
+      self.group.add_viewer self.user
+    end
+  end
+
+  def switch_edit
     if can_edit?
       self.can_edit = false
+      unless self.can_view?
+        self.destroy
+      end
     else
-      self.can_edit = true
+      self.group.add_editor self.user
     end
   end
 end
