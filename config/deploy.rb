261c30f44566bbd28b2d4fe672f7c3509948a000
Jan Suchal <johno@jsmf.net>
2009-03-17 14:39:10 +0100

    Added deploy config.

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "bonsai"
+set :repository,  "http://bitbucket.org/johno/bonsai/"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/rails/#{application}"
+set :user, "bonsai"
+set :use_sudo, false
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :mercurial
+
+server "nimbus.fiit.stuba.sk", :app, :web, :db
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+namespace :deploy do
+  task "start", :roles => :app do
+    passenger.restart
+  end
+
+  task "restart", :roles => :app do
+    passenger.restart
+  end
+end 
