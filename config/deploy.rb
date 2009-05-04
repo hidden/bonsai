6c3564c485b8cec169a96c3f28e4d035bde94ba8
Jan Suchal <johno@jsmf.net>
2009-05-04 16:43:58 +0200

    Only create shared path.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ namespace :deploy do
 
   desc "Symlink shared"
   task :symlink_shared do
-    run "mkdir -p #{release_path}/shared/upload"
+    run "mkdir #{release_path}/shared"
     run "ln -nfs #{shared_path}/upload #{release_path}/shared/upload"
   end
 
