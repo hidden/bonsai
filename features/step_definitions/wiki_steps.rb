037275ee98ced29ceffc0b69b62dd94f94d6ca94
Martin Repta <martinerko@gmail.com>
2009-10-14 23:41:35 +0200

    testy

--- features/step_definitions/wiki_steps.rb
+++ features/step_definitions/wiki_steps.rb
@@ -44,3 +44,25 @@ Given /^that a "(.*) page with multiple revisions exist$/ do |page|
   page_part.save!
 end
 
+
+
+#vytvorenie novej skupiny
+#when i create group nazov novej group
+When /^I create "(.*)" group$/ do |group_name|
+  click_link('Groups')
+  click_link('New group')
+  fill_in('group_name', :with => group_name)
+  click_button('Create')
+end
+
+
+
+Given /^group "\/?(.*)\/?" is viewable by "(.*)"$/ do |url, group|
+  page = Page.find_by_path(url.split("/"))
+  page.add_viewer Group.find_by_name(group)
+end
+
+Given /^group "\/?(.*)\/?" is editable by "(.*)"$/ do |url, group|
+  page = Page.find_by_path(url.split("/"))
+  page.add_editor Group.find_by_name(group)
+end
