50aa5ea620e7e91a95679630208697abaff51917
hidden <peter.student@gmail.com>
2009-11-14 18:14:00 +0100

    OpenID basic implementation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
   def login
-    return unless params[:username]
-    authenticator = Rails.env.production? ? SimpleLDAP : SimpleLDAP::Stub
-    data = authenticator.authenticate(params[:username], params[:password], 'ldap.stuba.sk', 389, 'ou=People,dc=stuba,dc=sk')
-    if data.nil?
-      flash[:error] = t(:login_error)
+    if APP_CONFIG['authentication_method'] == 'openid' then
+      open_id_authentication
     else
-      user = User.find_or_create_by_username(:username => params[:username], :name => data['cn'].first)
-      session[:user_id] = user.id
-      cookies[:token] = {:value => user.token, :expires => 1.month.from_now}
-      flash[:notice] = t(:login_successful)
+      return unless params[:username]
+      authenticator = Rails.env.production? ? SimpleLDAP : SimpleLDAP::Stub
+      data = authenticator.authenticate(params[:username], params[:password], 'ldap.stuba.sk', 389, 'ou=People,dc=stuba,dc=sk')
+      if data.nil?
+        failed_login
+      else
+        user = User.find_or_create_by_username(:username => params[:username], :name => data['cn'].first)
+        successful_login(user)
+      end
     end
-    redirect_to :back
   end
 
   def logout
@@ -21,4 +22,33 @@ class UsersController < ApplicationController
     redirect_to Page.root.get_path unless Page.root.nil?
     redirect_to "/" if Page.root.nil?
   end
+
+  private
+
+  def open_id_authentication
+    identity_url = params[:openid_identifier]
+    authenticate_with_open_id(identity_url, :optinal => [ :nickname ] ) do |result, identity_url, profile|
+      if result.successful?
+        name = profile['nickname'] || "openid"
+        user = User.find_or_create_by_username(:username => identity_url, :name => name)
+        successful_login(user)
+      else
+        failed_login
+      end
+    end
+  end
+
+  def successful_login(user)
+    session[:user_id] = user.id
+    cookies[:token] = {:value => user.token, :expires => 1.month.from_now}
+    flash[:notice] = t(:login_successful)
+    redirect_to :back
+  end
+
+  def failed_login
+    flash[:error] = t(:login_error)
+    redirect_to :back
+  end
+  
 end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
- def login_path
+  def login_path
     url_for :controller => 'users', :action => 'login'
   end
 
@@ -19,31 +19,31 @@ module ApplicationHelper
   def upload_file_path(page)
     "#{page.get_path};upload"
   end
-  
+
   def page_history_path(page)
    "#{page.get_path};show_history"
   end
-  
+
   def manage_page_path(page)
     "#{page.get_path};manage"
   end
-  
+
   def list_files_path(page)
     "#{page.get_path};files"
   end
-  
+
   def remove_permission_path(page, index)
     "#{page.get_path}?remove_permission&index=#{index}"
   end
-  
-  def change_permission_path(page,index,permission)
+
+  def change_permission_path(page, index, permission)
     "#{page.get_path};change_permission?index=#{index}&permission=#{permission}"
   end
 
   def switch_public_path(page)
     "#{page.get_path};switch_public"
   end
-  
+
   def switch_editable(page)
     "#{@page.get_path};switch_editable"
   end
@@ -51,9 +51,9 @@ module ApplicationHelper
   def view_page_path(page)
     page.get_path
   end
-  
+
   def summary_page_path(page)
-   "#{page.get_path};pagesib"
+    "#{page.get_path};pagesib"
   end
 
   def groups_page_path(page)
@@ -62,5 +62,13 @@ module ApplicationHelper
 
   def markdown(text)
     text.blank? ? "" : Maruku.new(text).to_html
+  end
+
+  def login_form
+    if APP_CONFIG['authentication_method'] == 'openid' then
+      render :partial => 'shared/openid_login'
+    else
+      render :partial => 'shared/ldap_login'
+    end
   end
 end
