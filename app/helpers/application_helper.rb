118179437a07074fc997fb823204e0a44ce968bf
Jan Suchal <johno@jsmf.net>
2010-02-10 17:47:07 +0100

    Special characters highlighting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,33 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-   def markdown(text)
-    text.blank? ? "" :  highlight(Maruku.new(text).to_html)
+  def markdown(text)
+    text.blank? ? "" : syntax_highlight(Maruku.new(text).to_html)
   end
 
-  def highlight(html)
-    html.gsub(/(<pre class='[a-z0-9-]+)/,'\1;').gsub("<pre class='","<pre class='brush:").gsub(/<.?code>/," ").gsub("<pre ",'<pre ')
+  def syntax_highlight(html)
+    html.gsub(/(<pre class='[a-z0-9-]+)/, '\1;').gsub("<pre class='", "<pre class='brush:").gsub(/<.?code>/, " ").gsub("<pre ", '<pre ')
   end
 
-  def highlight_match(string, infix)
-    # TODO highlight with special characters ján with jan, jan with ján
-    string.gsub(/#{infix}/i, '<strong>\0</strong>')
+  def highlight(text, phrases, *args)
+    options = args.extract_options!
+    unless args.empty?
+      options[:highlighter] = args[0] || '<strong class="highlight">\1</strong>'
+    end
+    options.reverse_merge!(:highlighter => '<strong class="highlight">\1</strong>')
+
+    if text.blank? || phrases.blank?
+      text
+    else
+      haystack = text.clone
+      match = Array(phrases).map { |p| Regexp.escape(p) }.join('|')
+      if options[:ignore_special_chars]
+        haystack = haystack.mb_chars.normalize(:kd)
+        match = match.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]+/n, '').gsub(/\w/, '\0[^\x00-\x7F]*')
+      end
+      highlighted = haystack.gsub(/(#{match})(?!(?:[^<]*?)(?:["'])[^<>]*>)/i, options[:highlighter])
+      highlighted = highlighted.mb_chars.normalize(:kc) if options[:ignore_special_chars]
+      return highlighted
+    end
   end
 
   def login_form
@@ -22,7 +39,7 @@ module ApplicationHelper
   end
 
   def icon_tag(source, options = {})
-    defaults = {:alt => "", :size => "16x16"}    
+    defaults = {:alt => "", :size => "16x16"}
     image_tag("icons/#{source}", defaults.merge(options))
   end
 end
