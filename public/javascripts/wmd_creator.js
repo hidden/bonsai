fc00e380cbb79e3eba8bf05a6caeaafa850e8612
Peter Student <peter.student@gmail.com>
2010-04-06 14:36:22 +0200

    WMD fix - first try.

--- public/javascripts/wmd_creator.js
+++ public/javascripts/wmd_creator.js
@@ -12,7 +12,7 @@ function createInstance(textarea_id)
     var textarea = document.getElementById(textarea_id);
     /***** build the preview manager *****/
     var panes = {input:textarea, preview:null, output:null};
-    var previewManager = new Attacklab.wmd.previewManager(panes);
+    //var previewManager = new Attacklab.wmd.previewManager(panes);
     /***** build the editor and tell it to refresh the preview after commands *****/
-    var editor = new Attacklab.wmd.editor(textarea,previewManager.refresh);
+    var editor = new Attacklab.wmd(textarea);
 }
