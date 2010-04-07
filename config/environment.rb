24b05d7828f051aba87a5e65da0db8f0ffe12d7e
Jan Suchal <johno@jsmf.net>
2010-04-08 00:22:57 +0200

    Added newrelic.

--- config/environment.rb
+++ config/environment.rb
@@ -43,6 +43,7 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '>=2.3.11'
   config.gem 'thinking-sphinx', :lib => 'thinking_sphinx', :version => '>=1.2'
   config.gem 'hoptoad_notifier'
+  config.gem "newrelic_rpm"
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
